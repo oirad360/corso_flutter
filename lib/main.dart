5bb84918ef91a4607edffb42bf0b4ddd764c03e4
d.anzalone <d.anzalone@reply.it>
2024-03-22 15:26:25 +0100

    column

--- lib/main.dart
+++ lib/main.dart
@@ -91,7 +91,8 @@ class _MyHomePageState extends State<MyHomePage> {
               fontFamily: 'LoversQuarrel', fontWeight: FontWeight.w600),
         ),
       ),
-      body: Center(
+      body: Container(
+        color: Colors.red,
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
@@ -108,7 +109,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center, // simile al justify content di css
+          crossAxisAlignment: CrossAxisAlignment.center, // simile al align-items di css
+          mainAxisSize: MainAxisSize.max, // occupa tutto lo spazio possibile
           children: <Widget>[
             // Image(image: AssetImage('images/spiaggia_dipinto.jpg')), // inserire immagine dichiarata in pubspec.yaml (primo modo)
             // Image.asset('images/spiaggia_dipinto.jpg'), // inserire immagine dichiarata in pubspec.yaml (secondo modo)
