1a7e57f6d6d644e8fef691c14dc18f010416b1f8
d.anzalone <d.anzalone@reply.it>
2024-03-27 16:35:12 +0100

    animazioni

--- lib/screens/SecondaPagina.dart
+++ lib/screens/SecondaPagina.dart
@@ -1,12 +1,24 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SecondaPagina extends StatelessWidget {
+class SecondaPagina extends StatefulWidget {
   const SecondaPagina(
       {super.key,
       required this.data}); // possiamo dichiarare nel costruttore eventuali dati aggiuntivi da passare al cambio pagina
 
   final String data;
+
+  @override
+  State<SecondaPagina> createState() => _SecondaPaginaState();
+}
+
+// convertiamo la pagina in StatefulWidget per poter gestire le proprietà da passare all' AnimatedContainer
+class _SecondaPaginaState extends State<SecondaPagina> {
+  double _width = 200; // '_' si usa per nominare variabili private
+  double _height = 200;
+  Color _color = Colors.blue;
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +33,26 @@ class SecondaPagina extends StatelessWidget {
               'Seconda Pagina',
               style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
-            Text(data, style: TextStyle(fontSize: 24)),
+            Text(widget.data, style: TextStyle(fontSize: 24)),
+            AnimatedContainer(
+                duration: const Duration(seconds: 1),
+                width: _width,
+                height: _height,
+                color: _color,
+                curve: Curves.fastOutSlowIn // si possono usare diverse curve per l'animazione
+            ),
+            // è un container che esegue un'animazione al cambio delle proprietà
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    final random = Random();
+                    _width = random.nextInt(300).toDouble();
+                    _height = random.nextInt(300).toDouble();
+                    _color = Color.fromRGBO(random.nextInt(256),
+                        random.nextInt(256), random.nextInt(256), 1);
+                  });
+                },
+                child: const Text('Randomizza'))
           ]),
         ),
         floatingActionButton: FloatingActionButton(
@@ -29,7 +60,7 @@ class SecondaPagina extends StatelessWidget {
             Navigator.pushNamed(context, '/prima');
           },
           tooltip: 'Cambia pagina',
-          child: Icon(Icons.change_circle),
+          child: const Icon(Icons.change_circle),
         ));
   }
 }
