4a22234a57798bb3e2327720f29d3a255c3a4958
d.anzalone <d.anzalone@reply.it>
2024-04-03 17:24:19 +0200

    clean main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,51 @@
 import 'package:flutter/material.dart';
-
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final _messengerKey = GlobalKey<ScaffoldMessengerState>();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      scaffoldMessengerKey: _messengerKey,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true
+        useMaterial3: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          foregroundColor: Colors.white,
-          title: const Text('Flutter Demo'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(
+          widget.title
         ),
-        body: const Center(
-          child: Column(
-            children: [
-              Chip(
-                avatar: CircleAvatar(
-                  backgroundColor: Colors.green,
-                  child: Text('LR'),
-                ),
-                label: Text('Luca Rossi'),
-              ),
-              CircularProgressIndicator()
-            ],
-          )
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            final snackbar = SnackBar(
-              content: const Text('Yay! A SnackBar!'),
-              action: SnackBarAction(
-                label: 'Undo',
-                onPressed: () {
-                  // Some code to undo the change.
-                },
-              ),
-            );
-            _messengerKey.currentState?.showSnackBar(snackbar);
-          },
-          tooltip: 'Show snackbar',
-          child: Icon(Icons.add),
-        )
-      )
+      ),
+      body: null,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+
+        },
+        tooltip: 'Show snackbar',
+        child: Icon(Icons.add),
+      ) // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
