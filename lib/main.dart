00df90e57596c6a936e95baecd5cee7a607a93a7
d.anzalone <d.anzalone@reply.it>
2024-05-07 14:13:09 +0200

    implementazione geolocalizzazione

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:location/location.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -77,7 +78,32 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () async {
+            Location location = new Location();
+
+            bool _serviceEnabled;
+            PermissionStatus _permissionGranted;
+            LocationData _locationData;
+
+            _serviceEnabled = await location.serviceEnabled();
+            if (!_serviceEnabled) {
+              _serviceEnabled = await location.requestService();
+              if (!_serviceEnabled) {
+                return;
+              }
+            }
+
+            _permissionGranted = await location.hasPermission();
+            if (_permissionGranted == PermissionStatus.denied) {
+              _permissionGranted = await location.requestPermission();
+              if (_permissionGranted != PermissionStatus.granted) {
+                return;
+              }
+            }
+
+            _locationData = await location.getLocation();
+            print(_locationData);
+          },
           child: Icon(Icons.add),
         ) // This trailing comma makes auto-formatting nicer for build methods.
     );
