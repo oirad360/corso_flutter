61aa5209442f968e94e60d0470d5da574634c01b
d.anzalone <d.anzalone@reply.it>
2024-05-07 16:14:18 +0200

    gestione orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
-import 'package:corso_flutter/screens/AuthPage.dart';
-import 'package:corso_flutter/services/auth.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
+import 'models/album.dart';
+import 'dart:convert'; // per utilizzare i metodi di 'json' (es. json.decode)
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+Future<http.Response> fetchData() { // Future è un tipo di dato asincrono
+  return http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
+}
+
+Future<List<Album>> fetchAlbums() async {
+  List<Album> albums = [];
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums'));
+  var body = json.decode(response.body); // body sarà una lista di mappe, ma non lo sappiamo a priori quindi usiamo var
+  for(var i=0; i<body.length; i++) {
+    albums.add(Album.fromJson(body[i]));
+  }
+  return albums;
+}
+
+void main() {
   runApp(const MyApp());
 }
 
@@ -25,18 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: StreamBuilder( // StreamBuilder è un widget che permette di costruire un widget in base ai dati di uno Stream
-        stream: AuthService().userChanges, // lo StreamBuilder si aggancia ad uno Stream, in questo caso userChanges
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          } else if (snapshot.hasData) {
-            return const MyHomePage(title: 'Flutter Demo Home Page');
-          } else {
-            return const AuthPage();
-          }
-        },
-      )
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -51,13 +47,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late Future<List<Album>> albums;
 
-  Future<void> logOut() async {
-    try {
-      await AuthService().signOut();
-    } on FirebaseAuthException catch (e) {
-      print(e);
-    }
+  @override
+  void initState() { // le chiamate asincrone vanno fatte nel metodo initState
+    super.initState();
+    albums = fetchAlbums();
   }
 
   @override
@@ -67,46 +62,32 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Text('Welcome ${AuthService().user!.email}'),
-              ElevatedButton(onPressed: () {
-                logOut();
-              }, child: Text('Logout'))
-            ],
-          )
+        body: FutureBuilder( // FutureBuilder è un widget che permette di costruire un widget in base al risultato di una Future
+            future: albums, // la future che vogliamo aspettare è 'albums', che viene inizializzata dalla chiamata a fetchAlbums() nel metodo initState
+            builder: (context, snapshot) {
+              if(snapshot.hasData){
+                return OrientationBuilder(builder: (BuildContext context, Orientation orientation) {
+                  return GridView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text('[$index] ${snapshot.data![index].title}'),
+                      );
+                    },
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      childAspectRatio: 6,
+                      crossAxisCount: (orientation == Orientation.portrait) ? 1 : 2,
+                    ),
+                  );
+                });
+              }else if(snapshot.hasError){
+                return Text('${snapshot.error}');
+              }
+              return const CircularProgressIndicator();
+            }
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            Location location = new Location();
-
-            bool _serviceEnabled;
-            PermissionStatus _permissionGranted;
-            LocationData _locationData;
-
-            _serviceEnabled = await location.serviceEnabled();
-            if (!_serviceEnabled) {
-              _serviceEnabled = await location.requestService();
-              if (!_serviceEnabled) {
-                return;
-              }
-            }
-
-            _permissionGranted = await location.hasPermission();
-            if (_permissionGranted == PermissionStatus.denied) {
-              _permissionGranted = await location.requestPermission();
-              if (_permissionGranted != PermissionStatus.granted) {
-                return;
-              }
-            }
-
-            _locationData = await location.getLocation();
-
-            location.onLocationChanged.listen((LocationData currentLocation) {
-              print('cambio location: ${_locationData}');
-            });
-          },
+          onPressed: () {},
           child: Icon(Icons.add),
         ) // This trailing comma makes auto-formatting nicer for build methods.
     );
